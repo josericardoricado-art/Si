333618e1f75750a153c771241d2e7188287ce6c0
josericardoricado-art <josericardoricado@gmail.com>
2026-09-26 01:52:21 -0300

    Update server.py

--- server.py
+++ server.py
@@ -548,21 +548,21 @@ def generate_piper_audio(
     )
 
     try:
-        with wave.open(
-    str(output),
-    "wb",
-) as wav_file:
+                with wave.open(
+            str(output),
+            "wb",
+        ) as wav_file:
 
-    wav_file.setnchannels(1)
-    wav_file.setsampwidth(2)
-    wav_file.setframerate(
-        voice.config.sample_rate
-    )
+            wav_file.setnchannels(1)
+            wav_file.setsampwidth(2)
+            wav_file.setframerate(
+                voice.config.sample_rate
+            )
 
-    voice.synthesize_wav(
-        text,
-        wav_file,
-    )
+            voice.synthesize_wav(
+                text,
+                wav_file,
+            )
 
     except Exception as error:
         try:
